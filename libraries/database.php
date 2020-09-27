cc7ffb8f0295266d5f79b53a236cd6dd6410114b
Circus-Pygargus <richard.meuret@laposte.net>
2020-09-27 09:39:33 +0000

    transformation de Database.php en singleton

--- libraries/database.php
+++ libraries/database.php
@@ -3,6 +3,8 @@
 class Database
 {
 
+    private static $instance = null;
+
     /**
      * Return a database connection
      * 
@@ -10,10 +12,14 @@ class Database
      */
     public static function getPdo(): PDO
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=pooliorchamla;charset=utf8', 'root', 'online@2017', [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
-        return $pdo;
+        // on utilise self:: pour appeler une propriété static de cette classe
+        if (self::$instance === null) {
+            self::$instance = new PDO('mysql:host=localhost;dbname=pooliorchamla;charset=utf8', 'root', 'online@2017', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        }
+        
+        return self::$instance;
     }
 }
